0b592d7e460f6188a0271abaef32ee7c0936864f
jmerin93 <jmerin102@gmail.com>
2024-02-15 21:22:59 +0530

    First Project for git

---
+++ src/test/java/com/cts/B.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+public class B {
+	public void t11() {
+		System.out.println("11");
+	}
+	public void t12() {
+		System.out.println("12");
+	}
+	public void t13() {
+		System.out.println("13");
+	}
+}
---
+++ src/test/java/com/hcl/A.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+public class A {
+	public void t1() {
+		System.out.println("1");
+	}
+	public void t2() {
+		System.out.println("2");
+	}
+	public void t3() {
+		System.out.println("3");
+	}
+}
