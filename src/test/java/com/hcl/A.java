8a1e329d3aa415becc1ba4488f244d104dabf045
jmerin93 <jmerin102@gmail.com>
2024-02-16 21:53:15 +0530

    Changes made to class B

--- src/test/java/com/hcl/A.java
+++ src/test/java/com/hcl/A.java
@@ -10,4 +10,13 @@ public class A {
 	public void t3() {
 		System.out.println("3");
 	}
+	public void a1() {
+		System.out.println("1");
+	}
+	public void a2() {
+		System.out.println("2");
+	}
+	public void a3() {
+		System.out.println("3");
+	}
 }
