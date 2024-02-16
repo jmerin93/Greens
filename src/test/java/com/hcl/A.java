c4c50c4981eb3345f0e86785dcbffcacd82a8b3f
jmerin93 <jmerin102@gmail.com>
2024-02-16 21:39:48 +0530

    Changes made to class A

--- src/test/java/com/hcl/A.java
+++ src/test/java/com/hcl/A.java
@@ -10,4 +10,13 @@ public class A {
 	public void t3() {
 		System.out.println("3");
 	}
+	public void m1() {
+		System.out.println("1");
+	}
+	public void m2() {
+		System.out.println("2");
+	}
+	public void m3() {
+		System.out.println("3");
+	}
 }
